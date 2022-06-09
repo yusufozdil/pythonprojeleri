f01bfeeac612c040e887a891a0c4818079115953
yusufozdil <yusuf.ozdil345272@gmail.com>
2022-06-09 16:40:57 +0300

    Giriş Sistemi Denemeleri

---
+++ girisSistemi.py
@@ -0,0 +1,37 @@
+kayitliKullanıcılar=["Yusuf"]
+yenikullanici=["Selami"]
+soru=input("Kayıtlı Bir Kullanıcı Mısınız?(y/n): ")
+if soru=="y":
+
+        isim=input("İsminiz Nedir? ")
+        if isim in kayitliKullanıcılar:
+            print("Hoşgeldiniz! ")
+        else:
+            print("Kayıtlı Kullanıcı Bulunamadı. ")
+
+elif soru=="n":
+        kayıtSoru=input("Kayıt Olmak İster Misiniz?(y/n): ")
+        
+        if kayıtSoru=="y":
+            isim=input("İsminiz Nedir? ")
+
+            if isim in kayitliKullanıcılar:
+                print("Zaten Kayıtlı Bir Kullanıcısınız. ")
+
+            else:
+                yenikullanici.append(isim)    
+                print("Hoşgeldiniz! ")
+
+        elif kayıtSoru=="n":
+            print("Sağ Yukarıdaki Çarpıya Bas Cınım ")
+
+        else:
+            print("Lütfen Geçerli Bir Cevap Girin")
+
+else:
+        print("Progrramı Tekrar Çalıştır ve Ne Yazdığımı Oku. ")
+
+kayitliKullanıcılar.extend(yenikullanici)
+print(kayitliKullanıcılar)
+
+
